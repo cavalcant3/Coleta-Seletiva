aa2b8ea1f886643a6d42a6449628e049da505b9a
cavalcant3 <jmatheusoc269@gmail.com>
2021-10-17 16:21:05 -0300

    First commit

---
+++ coleta-seletiva/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <h1>
+          eai brasil
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
