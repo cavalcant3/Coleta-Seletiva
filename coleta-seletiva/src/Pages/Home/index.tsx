77d78f4b257b34d90eba6f15246de7b7c9e41571
cavalcant3 <jmatheusoc269@gmail.com>
2021-10-18 01:35:44 -0300

    página inicial

--- coleta-seletiva/src/Pages/Home/index.tsx
+++ coleta-seletiva/src/Pages/Home/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import './styles.css';
+ import './styles.css';
+import { FiLogIn } from 'react-icons/fi'
+import logo from '../../assets/logo.svg'
 
 const index: React.FC= () => {
   
     return (
-      <div>
-        matheus na area
+      <div id="page-home">
+        <div className="content">
+            <header>
+              <img src={logo} alt="Reciclagem" />
+            </header>
+            <main>
+              <h1>Coleta Seletiva e reciclagem em geral.</h1>
+              <p>Reciclagem de materiais diversos, tais como, papel, plástico, metais, entre outros</p>
+              <a href="/create-location">
+                <span>
+                  <FiLogIn />
+                </span>
+                <strong>Cadastrar novo local de coleta</strong>
+              </a>
+            </main>
+        </div>
+        
       </div>
     );
   
